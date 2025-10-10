71d90612e1cf97c669e9a02eca2196ca0f5bd78a
KADEEJARASHA <kadeejarasha10@gmail.com>
2025-10-10 16:05:19 +0530

    unbundle

---
+++ kalyan/kalyan/doctype/unbundling/unbundling.js
@@ -0,0 +1,316 @@
+
+
+
+
+
+
+
+
+
+
+
+
+    // custom_locker_select(frm) {
+    //     if (frm.doc.docstatus === 1) {
+    //         frappe.msgprint("You cannot transfer after submitting the Unbundling document.");
+    //         return;
+    //     }
+
+    //     frm.set_value("custom_custom_items_visible", 1);
+
+    //     // Track number of transfers
+    //     frm._transfer_count = frm._transfer_count || 0;
+    //     frm._transfer_count += 1;
+
+    //     // Save previous selection as read-only if exists
+    //     if (frm.doc.custom_selected_items && frm.doc.custom_selected_items.length) {
+    //         frm.set_df_property("custom_selected_items", "read_only", 1);
+    //     }
+
+    //     // Create a fresh selection table for this transfer
+    //     frm.clear_table("custom_selected_items");
+
+    //     // Exclude already transferred items
+    //     const alreadyTransferred = (frm.doc.custom_transferred_items || []).map(d => d.item_code);
+
+    //     (frm.doc.packing_items || []).forEach(item => {
+    //         if (!alreadyTransferred.includes(item.item_code)) {
+    //             let row = frm.add_child("custom_selected_items");
+    //             row.item_code = item.item_code;
+    //             row.item_name = item.item_name;
+    //             row.qty = item.qty;
+    //             row.uom = item.uom;
+    //             row.serial_no = item.serial_no;
+    //         }
+    //     });
+
+    //     frm.refresh_field("custom_selected_items");
+
+
+
+
+
+
+// frappe.ui.form.on("Unbundling", {
+//     bundle(frm) {
+//         if (frm.doc.bundle) {
+//             frappe.call({
+//                 method: "kalyan.kalyan.doctype.unbundling.unbundling.get_bundle_details",
+//                 args: { bundle: frm.doc.bundle },
+//                 callback(r) {
+//                     if (r.message) {
+//                         frm.clear_table("packing_items");
+//                         (r.message.items || []).forEach(item => {
+//                             let row = frm.add_child("packing_items");
+//                             row.item_code = item.item_code;
+//                             row.item_name = item.item_name;
+//                             row.qty = item.qty;
+//                             row.uom = item.uom;
+//                             row.serial_no = item.serial_no || "";
+//                         });
+//                         frm.refresh_field("packing_items");
+//                     }
+//                 }
+//             });
+//         }
+//     },
+
+
+
+
+// custom_locker_select(frm) {
+//     if (frm.doc.docstatus === 1) {
+//         frappe.msgprint("You cannot transfer after submitting the Unbundling document.");
+//         return;
+//     }
+
+//     frm.set_value("custom_custom_items_visible", 1);
+
+//     frm._transfer_count = frm._transfer_count || 0;
+//     frm._transfer_count += 1;
+
+//     if (frm.doc.custom_selected_items && frm.doc.custom_selected_items.length) {
+//         frm.set_df_property("custom_selected_items", "read_only", 1);
+//     }
+
+//     frm.clear_table("custom_selected_items");
+
+//     const alreadyTransferred = (frm.doc.custom_transferred_items || []).map(d => d.item_code);
+
+//     let newItemsAdded = false;
+//     (frm.doc.packing_items || []).forEach(item => {
+//         if (!alreadyTransferred.includes(item.item_code)) {
+//             let row = frm.add_child("custom_selected_items");
+//             row.item_code = item.item_code;
+//             row.item_name = item.item_name;
+//             row.qty = item.qty;
+//             row.uom = item.uom;
+//             row.serial_no = item.serial_no;
+//             newItemsAdded = true;
+//         }
+//     });
+
+//     frm.refresh_field("custom_selected_items");
+
+//     if (!newItemsAdded) {
+//         frappe.msgprint("All items have already been transferred.");
+//         frm.set_df_property("custom_selected_items", "hidden", 1);
+//         return;
+//     } else {
+//         frm.set_df_property("custom_selected_items", "hidden", 0);
+//     }
+
+    
+
+
+
+
+//     frm.add_custom_button("Transfer", () => {
+//             if (frm.doc.docstatus === 1) {
+//                 frappe.msgprint("You cannot transfer after submitting the Unbundling document.");
+//                 return;
+//             }
+
+//             if (frm.is_dirty()) {
+//                 frm.save().then(() => create_stock_transfer(frm));
+//             } else {
+//                 create_stock_transfer(frm);
+//             }
+//         });
+
+//     },
+
+//     refresh(frm) {
+//         frm.set_df_property("custom_selected_items", "hidden", !frm.doc.custom_custom_items_visible);
+
+//         if (frm.doc.docstatus === 1) {
+//             frm.set_df_property("custom_locker_select", "read_only", 1);
+//             frm.set_df_property("custom_selected_items", "read_only", 1);
+//         }
+//     }
+// });
+
+
+
+// function create_stock_transfer(frm) {
+//     frappe.confirm(
+//         "Are you sure you want to create a Stock Entry for the selected items?",
+//         () => {
+//             frappe.call({
+//                 method: "kalyan.kalyan.doctype.unbundling.unbundling.create_stock_entry_from_unbundling",
+//                 args: { docname: frm.doc.name },
+//                 freeze: true,
+//                 freeze_message: "Creating Stock Entry...",
+//                 callback(r) {
+//                     if (!r.exc) {
+//                         frappe.msgprint({
+//                             title: "Success",
+//                             message: " Stock Entry created successfully.",
+//                             indicator: "green"
+//                         });
+
+                    
+//                         if (!frm.doc.custom_transferred_items) {
+//                             frm.set_value("custom_transferred_items", []);
+//                         }
+//                         const transferred = frm.doc.custom_selected_items.map(d => ({
+//                             item_code: d.item_code
+//                         }));
+//                         frm.doc.custom_transferred_items.push(...transferred);
+//                         frm.refresh_field("custom_transferred_items");
+
+                    
+//                         frm.clear_table("custom_selected_items");
+//                         frm.refresh_field("custom_selected_items");
+//                     }
+//                 }
+//             });
+//         }
+//     );
+// }
+
+
+frappe.ui.form.on("Unbundling", {
+    bundle(frm) {
+        if (frm.doc.bundle) {
+            frappe.call({
+                method: "kalyan.kalyan.doctype.unbundling.unbundling.get_bundle_details",
+                args: { bundle_creator_name: frm.doc.bundle },
+                callback(r) {
+                    if (r.message) {
+                        frm.clear_table("packing_items");
+                        (r.message.items || []).forEach(item => {
+                            let row = frm.add_child("packing_items");
+                            row.item_code = item.item_code;
+                            row.item_name = item.item_name;
+                            row.qty = item.qty;
+                            row.uom = item.uom;
+                            // row.serial_no = item.serial_no || "";
+                        });
+                        frm.refresh_field("packing_items");
+                    }
+                }
+            });
+        }
+    },
+
+    custom_locker_select(frm) {
+        if (frm.doc.docstatus === 1) {
+            frappe.msgprint("You cannot transfer after submitting the Unbundling document.");
+            return;
+        }
+
+        frm.set_value("custom_custom_items_visible", 1);
+        frm._transfer_count = frm._transfer_count || 0;
+        frm._transfer_count += 1;
+
+        if (frm.doc.custom_selected_items && frm.doc.custom_selected_items.length) {
+            frm.set_df_property("custom_selected_items", "read_only", 1);
+        }
+
+        frm.clear_table("custom_selected_items");
+
+        const alreadyTransferred = (frm.doc.custom_transferred_items || []).map(d => d.item_code);
+
+        let newItemsAdded = false;
+        (frm.doc.packing_items || []).forEach(item => {
+            if (!alreadyTransferred.includes(item.item_code)) {
+                let row = frm.add_child("custom_selected_items");
+                row.item_code = item.item_code;
+                row.item_name = item.item_name;
+                row.qty = item.qty;
+                row.uom = item.uom;
+                row.serial_no = item.serial_no;
+                newItemsAdded = true;
+            }
+        });
+
+        frm.refresh_field("custom_selected_items");
+
+        if (!newItemsAdded) {
+            frappe.msgprint("All items have already been transferred.");
+            frm.set_df_property("custom_selected_items", "hidden", 1);
+            return;
+        } else {
+            frm.set_df_property("custom_selected_items", "hidden", 0);
+        }
+
+        frm.add_custom_button("Transfer", () => {
+            if (frm.doc.docstatus === 1) {
+                frappe.msgprint("You cannot transfer after submitting the Unbundling document.");
+                return;
+            }
+
+            if (frm.is_dirty()) {
+                frm.save().then(() => create_stock_transfer(frm));
+            } else {
+                create_stock_transfer(frm);
+            }
+        });
+    },
+
+    refresh(frm) {
+        frm.set_df_property("custom_selected_items", "hidden", !frm.doc.custom_custom_items_visible);
+
+        if (frm.doc.docstatus === 1) {
+            frm.set_df_property("custom_locker_select", "read_only", 1);
+            frm.set_df_property("custom_selected_items", "read_only", 1);
+        }
+    }
+});
+
+function create_stock_transfer(frm) {
+    frappe.confirm(
+        "Are you sure you want to create a Stock Entry for the selected items?",
+        () => {
+            frappe.call({
+                method: "kalyan.kalyan.doctype.unbundling.unbundling.create_stock_entry_from_unbundling",
+                args: { docname: frm.doc.name },
+                freeze: true,
+                freeze_message: "Creating Stock Entry...",
+                callback(r) {
+                    if (!r.exc) {
+                        frappe.msgprint({
+                            title: "Success",
+                            message: "Materials Transferred to Locker.",
+                            indicator: "green"
+                        });
+
+                        if (!frm.doc.custom_transferred_items) {
+                            frm.set_value("custom_transferred_items", []);
+                        }
+
+                        const transferred = frm.doc.custom_selected_items.map(d => ({
+                            item_code: d.item_code
+                        }));
+                        frm.doc.custom_transferred_items.push(...transferred);
+                        frm.refresh_field("custom_transferred_items");
+
+                        frm.clear_table("custom_selected_items");
+                        frm.refresh_field("custom_selected_items");
+                    }
+                }
+            });
+        }
+    );
+}
---
+++ kalyan/kalyan/doctype/unbundling/unbundling.py
@@ -0,0 +1,259 @@
+
+
+
+
+
+
+
+
+
+
+
+# import frappe
+# from frappe.model.document import Document
+
+# class Unbundling(Document):
+#     pass
+
+
+# @frappe.whitelist()
+# def get_bundle_details(bundle):
+#     """Fetch bundle items for Unbundling."""
+#     bundle_doc = frappe.get_doc("Bundle", bundle)
+#     return {
+#         "items": [
+#             {
+#                 "item_code": d.item or d.item_code,
+#                 "item_name": d.item_name,
+#                 "qty": d.qty,
+#                 "uom": d.uom,
+#                 "serial_no": getattr(d, "serial_no", "")
+#             }
+#             for d in bundle_doc.bundle_items
+#         ]
+#     }
+
+
+# @frappe.whitelist()
+# def create_stock_entry_from_unbundling(docname):
+#     doc = frappe.get_doc("Unbundling", docname)
+
+#     if doc.docstatus == 1:
+#         frappe.throw("Cannot create Stock Entry after Unbundling is submitted.")
+
+#     create_stock_entry_from_unbundling_internal(doc)
+#     frappe.db.commit()
+#     return "Stock Entry Created"
+
+
+
+
+
+# def create_stock_entry_from_unbundling_internal(doc):
+#     """Create Material Transfer Stock Entry from selected items."""
+#     if not doc.custom_selected_items:
+#         frappe.throw("No items selected for transfer.")
+
+#     se = frappe.new_doc("Stock Entry")
+#     se.stock_entry_type = "Material Transfer"
+#     se.from_warehouse = doc.source_warehouse
+#     se.to_warehouse = doc.locker
+
+#     for d in doc.custom_selected_items:
+#         item_doc = frappe.get_doc("Item", d.item_code)
+
+#         if item_doc.has_serial_no:
+#             if not d.serial_no:
+#                 frappe.throw(f"Serial No required for serialized item {d.item_code}")
+
+#             serials = [s.strip() for s in d.serial_no.split(",")]
+#             for sn in serials:
+#                 se.append("items", {
+#                     "item_code": d.item_code,
+#                     "item_name": d.item_name,
+#                     "qty": 1,
+#                     "uom": d.uom,
+#                     "s_warehouse": doc.source_warehouse,
+#                     "t_warehouse": doc.locker,
+#                     "serial_no": sn
+#                 })
+
+#         elif item_doc.has_batch_no:
+#             if not d.serial_no:
+#                 frappe.throw(f"Batch No required for batch-tracked item {d.item_code}")
+
+#             se.append("items", {
+#                 "item_code": d.item_code,
+#                 "item_name": d.item_name,
+#                 "qty": d.qty,
+#                 "uom": d.uom,
+#                 "s_warehouse": doc.source_warehouse,
+#                 "t_warehouse": doc.locker,
+#                 "batch_no": d.serial_no
+#             })
+
+#         else:
+#             se.append("items", {
+#                 "item_code": d.item_code,
+#                 "item_name": d.item_name,
+#                 "qty": d.qty,
+#                 "uom": d.uom,
+#                 "s_warehouse": doc.source_warehouse,
+#                 "t_warehouse": doc.locker
+#             })
+ 
+#     se.insert()
+#     se.submit()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+import frappe
+from frappe.model.mapper import get_mapped_doc
+from frappe.model.document import Document
+
+class Unbundling(Document):
+    pass
+
+
+# @frappe.whitelist()
+# # def make_unbundling_from_received_bundle(source_name, target_doc=None):
+# #     print(88888888888888888888)
+# #     """
+# #     Maps a Received Bundle to a new Unbundling document,
+# #     including bundles -> packing_items
+# #     """
+
+# def make_unbundling_from_received_bundle(source_name, target_doc=None):
+
+#   doc = get_mapped_doc(
+#     "Received Bundle",
+#     source_name,
+#         {
+#             "Received Bundle": {
+#                 "doctype": "Unbundling"
+#             },
+#         },
+#         target_doc
+#     )
+#   return doc
+
+    # # return get_mapped_doc(
+    # #     "Received Bundle",
+    # #     source_name,
+    # #     {
+    # #         "Received Bundle": {
+    # #             "doctype": "Unbundling",
+    # #             "field_map": {
+    # #                 "name": "received_bundle",
+    # #                 "from_warehouse": "source_warehouse",
+    # #                 "to_warehouse": "locker"  # rename target warehouse field
+    # #             }
+    # #         },
+    # #         "Bundles": {  # child table mapping
+    # #             "doctype": "Packing Items",
+    # #             "field_map": {
+    # #                 "bundle": "bundle"
+    # #             }
+    # #         }
+    #     },
+    #     target_doc
+    # )
+
+
+@frappe.whitelist()
+def get_bundle_details(bundle_creator_name):
+    """Fetch all items from Packet Generator inside the selected Bundle Creator."""
+    bundle_creator = frappe.get_doc("Bundle Creator", bundle_creator_name)
+    
+    all_items = []
+    for packet in bundle_creator.packet_items:
+        packet_doc = frappe.get_doc("Packet Generator", packet.packet_id)
+        for item in packet_doc.items:
+            all_items.append({
+                "item_code": item.item_code,      
+                "item_name": item.item_name,
+                "qty": item.qty,
+                "uom": item.uom,
+                "serial_no": getattr(item, "serial_no", "")
+            })
+    
+    return {"items": all_items}
+
+@frappe.whitelist()
+def create_stock_entry_from_unbundling(docname):
+    doc = frappe.get_doc("Unbundling", docname)
+
+    if doc.docstatus == 1:
+        frappe.throw("Cannot create Stock Entry after Unbundling is submitted.")
+
+    create_stock_entry_from_unbundling_internal(doc)
+    frappe.db.commit()
+    return "Stock Entry Created"
+
+
+def create_stock_entry_from_unbundling_internal(doc):
+    """Create Material Transfer Stock Entry from selected items."""
+    if not doc.custom_selected_items:
+        frappe.throw("No items selected for transfer.")
+
+    se = frappe.new_doc("Stock Entry")
+    se.stock_entry_type = "Material Transfer"
+    se.from_warehouse = doc.source_warehouse
+    se.to_warehouse = doc.locker
+
+    for d in doc.custom_selected_items:
+        item_doc = frappe.get_doc("Item", d.item_code)
+
+        if item_doc.has_serial_no:
+            if not d.serial_no:
+                frappe.throw(f"Serial No required for serialized item {d.item_code}")
+            serials = [s.strip() for s in d.serial_no.split(",")]
+            for sn in serials:
+                se.append("items", {
+                    "item_code": d.item_code,
+                    "item_name": d.item_name,
+                    "qty": 1,
+                    "uom": d.uom,
+                    "s_warehouse": doc.source_warehouse,
+                    "t_warehouse": doc.locker,
+                    "serial_no": sn
+                })
+
+        elif item_doc.has_batch_no:
+            if not d.serial_no:
+                frappe.throw(f"Batch No required for batch-tracked item {d.item_code}")
+            se.append("items", {
+                "item_code": d.item_code,
+                "item_name": d.item_name,
+                "qty": d.qty,
+                "uom": d.uom,
+                "s_warehouse": doc.source_warehouse,
+                "t_warehouse": doc.locker,
+                "batch_no": d.serial_no
+            })
+
+        else:
+            se.append("items", {
+                "item_code": d.item_code,
+                "item_name": d.item_name,
+                "qty": d.qty,
+                "uom": d.uom,
+                "s_warehouse": doc.source_warehouse,
+                "t_warehouse": doc.locker
+            })
+
+    se.insert()
+    se.submit()
